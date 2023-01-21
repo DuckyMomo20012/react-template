778f1257058e2a184d3c10f3bc5033fa3e0d1366
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-01-21 10:40:39 +0700

    feat: migrate test files to ts

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test('homepage has dark mode demo section and dark mode button', async ({
   page,
