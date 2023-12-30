f774790b7f26dcdbeb015e78ef734b390c0762d9
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-12-30 10:23:53 +0700

    feat: update new UI

---
+++ src/components/modules/OverlayDemo.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import { Button, Tooltip } from '@mantine/core';
+import { memo } from 'react';
+import { Feature } from '@/components/elements/Feature';
+import { type ControlledDemoProps } from '@/pages/index';
+
+const OverlayDemo = memo(function OverlayDemo({
+  color,
+  disabled,
+  label,
+  radius,
+}: ControlledDemoProps) {
+  return (
+    <>
+      <Feature group="Overlays" title="Tooltip">
+        <Tooltip
+          disabled={disabled}
+          label={label}
+          opened
+          position="right"
+          radius={radius}
+        >
+          <Button className="w-min" color={color} variant="outline">
+            Button
+          </Button>
+        </Tooltip>
+        <Tooltip
+          color={color}
+          disabled={disabled}
+          label={label}
+          opened
+          position="right"
+          radius={radius}
+        >
+          <Button className="w-min" color={color} variant="outline">
+            Button
+          </Button>
+        </Tooltip>
+        <Tooltip
+          color={color}
+          disabled={disabled}
+          label={label}
+          opened
+          position="right"
+          radius={radius}
+          withArrow
+        >
+          <Button className="w-min" color={color} variant="outline">
+            Button
+          </Button>
+        </Tooltip>
+        <Tooltip
+          className="w-20"
+          color={color}
+          disabled={disabled}
+          label={label}
+          multiline
+          opened
+          position="bottom"
+          radius={radius}
+        >
+          <Button className="w-min" color={color} variant="outline">
+            Button
+          </Button>
+        </Tooltip>
+      </Feature>
+    </>
+  );
+});
+
+export { OverlayDemo };
---
+++ src/components/modules/TypographyDemo.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { Icon } from '@iconify/react';
+import { Blockquote, Code, Highlight, Stack, Title } from '@mantine/core';
+import { memo } from 'react';
+import { Feature } from '@/components/elements/Feature';
+import { type ControlledDemoProps } from '@/pages/index';
+
+const TypographyDemo = memo(function TypographyDemo({
+  color,
+  description,
+  label,
+  radius,
+}: ControlledDemoProps) {
+  return (
+    <>
+      <Feature group="Typography" title="Blockquote">
+        <Blockquote
+          cite={description}
+          color={color}
+          icon={
+            <Icon
+              height="70%"
+              icon="material-symbols:info-outline-rounded"
+              width="70%"
+            />
+          }
+          iconSize={32}
+          radius={radius}
+        >
+          {label}
+        </Blockquote>
+      </Feature>
+
+      <Feature group="Typography" title="Code">
+        <Stack className="w-min">
+          <Code>{label}</Code>
+          <Code c="white" color={color}>
+            {label}
+          </Code>
+        </Stack>
+      </Feature>
+
+      <Feature group="Typography" title="Highlight">
+        <Highlight highlight="this">
+          Highlight This, definitely THIS and also this!
+        </Highlight>
+        <Highlight color={color} highlight="this">
+          Highlight This, definitely THIS and also this!
+        </Highlight>
+      </Feature>
+
+      <Feature
+        className="col-span-1 md:col-span-2"
+        group="Typography"
+        title="Title"
+      >
+        <Title order={1}>{label}</Title>
+        <Title order={2}>{label}</Title>
+        <Title order={3}>{label}</Title>
+        <Title order={4}>{label}</Title>
+        <Title order={5}>{label}</Title>
+        <Title order={6}>{label}</Title>
+      </Feature>
+    </>
+  );
+});
+
+export { TypographyDemo };
